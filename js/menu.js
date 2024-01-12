5dc50e832f9485c20920bc253b674ff3d42c02ac
GreatValour <oluwanifemivalour@outlook.com>
2024-01-11 19:11:59 -0500

    first commit

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+	$(document).ready(function() {
+ 	$('ul.sf-menu').superfish({
+      delay:       800,                            
+      animation:   {opacity:'show', height:'show'}, 
+      speed:       'faster',                          
+      autoArrows:  false,                   
+      dropShadows: false                         
+   })
+ });
